68c25f8c42f20921571cdd79b0cc85c0454a171b
DevDhaif <devdhaif@gmail.com>
2021-12-19 13:34:34 +0200

    moved index.html to the root folder

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,11 @@
 const debounce = (func,delay=1000) => {
-    let timeout;
-    return (...arg) => {
-      if (timeout) {
-        clearTimeout(timeout);
-      }
-      timeout = setTimeout(() => {
-        func.apply(null, arg);
-      }, delay);
-    };
+  let timeout;
+  return (...arg) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      func.apply(null, arg);
+    }, delay);
   };
+};
